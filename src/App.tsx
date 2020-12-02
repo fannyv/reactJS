132bb3eba595393497751a628a266741c7640a59
FVelsin <9PZfimmd5mh3BmIEV87a>
2020-12-02 13:42:57 +0100

    play with Button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import './App.css';
 import styled from "@emotion/styled";
-
+import {Button} from 'react-bootstrap';
 
 const Header = styled.div`
   background-color: #282c34;
@@ -13,7 +12,7 @@ const Header = styled.div`
   color: white;
 `
 
-const Button = styled.button`
+const ButtonStyled = styled.button<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>>`
   padding: 32px;
   background-color: hotpink;
   font-size: 24px;
@@ -25,11 +24,29 @@ const Button = styled.button`
   }
 `
 
+const buttonClick = () => {
+    alert("Test on click!");
+};
+
+const buttonReactClick = () => {
+    alert("Test on success button click!");
+};
+
+
 export const App = () => {
     return (
         <div>
             <Header>Fanny test le react</Header>
-            <Button>Fanny</Button>
+            <ButtonStyled onClick={buttonClick}>Fanny</ButtonStyled>
+            <Button variant="success" onClick={buttonReactClick}>Success</Button>
+
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="success">Success</Button>
+            <Button variant="warning">Warning</Button>
+            <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>
+            <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>
+            <Button variant="link">Link</Button>
         </div>
     );
 }
