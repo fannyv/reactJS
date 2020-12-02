683c844588e90b1efab4c91dde1a613606dd0303
FVelsin <9PZfimmd5mh3BmIEV87a>
2020-12-02 09:24:21 +0100

    issue with eslint?

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {App} from "./app/App";
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root') as HTMLElement
-);
+//Init App
+ReactDOM.render(<App/>, document.getElementById('root'));
